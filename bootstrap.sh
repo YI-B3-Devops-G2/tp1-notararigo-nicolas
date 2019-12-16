32f0f72a8e44ee5214451f2d028da47f4e1ceb61
Nicolas Notararigo <nicolas.notararigo@ynov.com>
2019-12-16 17:47:46 +0100

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,17 +3,10 @@
 echo -e '\033[0;34m Hello, \n installing a base of machine...'
 echo -e '\033[0m'
 
-sudo apt-get update 
-sudo apt-get -y upgrade
+sudo apt-get update | sudo apt-get -y upgrade
 
 echo -e "\033[0;32m Update success without error."
 echo -e "\033[0;34m Installing NodeJS@12 ... "
-echo -e '\033[0m'
-
-sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates | sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-
-echo -e "\033[0;32m Add Node.js APT Repository without error."
-echo -e "\033[0;34m Installing NodeJS..."
 echo -e '\033[0m'
 
 sudo apt -y install nodejs
@@ -28,10 +21,3 @@ echo -e "\033[0;32m Successfull installation without error. \a"
 echo -e '\033[0m'
 
 sudo /etc/init.d/nginx start
-
-echo "<!DOCTYPE html>
-<html>
-<body>
-B3 Devops - TP 1
-</body>
-</html>" >> /var/www/html/index.nginx-debian.html
